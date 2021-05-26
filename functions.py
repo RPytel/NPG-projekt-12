b9727684f2ad9c46db90bb2970092361ad974ad0
andreimek <anesc@student.agh.edu.pl>
2021-05-26 13:44:57 +0200

    skonczylem real_numbers i zakomentowalem maina, jak cos to pytac

---
+++ functions.py
@@ -0,0 +1,27 @@
+class real_number:
+    __slots__ = ['x','y','op']
+    def __init__(self,x,op,y ):
+
+        self.x=x
+        self.y=y
+        self.op=op
+    def __repr__(self): #metoda ktora wypisuje klase
+        return f"{self.x} {self.op} {self.y}"
+
+    def __add__(self): #metoda dodawania
+        return self.x + self.y
+
+    def __mul__(self): #metoda mnożenia
+        return self.x*self.y
+
+    def __sub__(self): #metoda odejmowania
+        return self.x-self.y
+
+    def __truediv__(self): #metoda dzielenia
+        return self.x / self.y
+
+    def __pow__(self): #metoda potęgowania
+        power = 1
+        for x in range(self.y):
+            power = power * self.x
+        return power
